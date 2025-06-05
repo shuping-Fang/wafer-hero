f0350a182dbbee79b4440456a4fa15c572670529
shuping-Fang <60771187+shuping-Fang@users.noreply.github.com>
2025-06-06 06:16:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ function renderCards(cardData) {
     card.className = 'card';
     card.draggable = true;
     card.dataset.id = step.id;
-    card.innerHTML = `<div>${step.name}</div>`;
+    card.innerHTML = `
+    <img src="assets/${step.image}" style="width:100%; border-radius:6px;"><br>
+    <strong>${step.name}</strong>;
     card.addEventListener('dragstart', dragStart);
     area.appendChild(card);
   });
